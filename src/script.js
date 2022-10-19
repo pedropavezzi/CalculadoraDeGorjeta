6802b912c4ec8d0797884d05210d1172df3219e6
Pedro Pavezzi <pedro.pavezzi@hotmail.com>
2022-10-19 21:17:42 +0100

    Consegui verificar o erro da condição para alterar as imagens de acordo com o atendimento. Era problema no sinal de comparação e depois na declaração do objeto

--- src/script.js
+++ src/script.js
@@ -1,37 +1,31 @@
-
-
-
 let inputValor = document.getElementById("valor");
 let inputAmount = document.getElementById("amount");
-let inputAtendimento = document.getElementById("atendimento");
-
+let inputAtendimento = document.getElementById("atendimento")
 let resultado = document.getElementById("resultado");
 let garcom = document.getElementById("garcom");
-let foto = document.getElementById("foto");
-
-
 
 function calcular() {
 
     let valor = inputValor.value;
     let amount = inputAmount.value;
     let atendimento = inputAtendimento.value;
-
     let gorjeta = (valor / amount) * atendimento / 100
 
     let totalGorjeta = gorjeta / amount
 
     resultado.innerHTML = `<p> R$ ${gorjeta} para cada pessoa na mesa</p>`
-    resultado.innerHTML += `<p> O valor total da gorjeta foi de  R$ ${totalGorjeta}</p>`
-    garcom.innerHTML += `<img  src=${foto} width="400" height="150"/>`
 
-    garcom.innerHTML += foto
+}
 
-    if (atendimento.value = 5) {
-        let foto = "./assets/triste.png"
-    } else if (atendimento.value = 10) {
-        let foto = "./assets/ok.png"
+function mudarFoto() {
+    let atendimento = inputAtendimento.value;
+
+    if (atendimento == 5) {
+        document.getElementById("foto").src = "./assets/triste.png";
+    } else if (atendimento == 10) {
+        document.getElementById("foto").src = "./assets/ok.png";
     } else {
-        let foto = "./assets/feliz.png"
+        document.getElementById("foto").src = "./assets/feliz.png";
     }
+
 }
